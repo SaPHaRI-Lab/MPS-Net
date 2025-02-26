9995eb17eee8f65cee93f2ae345e2c719a277345
Ashraf Ibraheem <ibraheemashraf10@gmail.com>
2025-02-26 13:43:41 -0500

    Joint A-A to Quat code file

--- quaternion_transform.py
+++ quaternion_transform.py
@@ -1,6 +1,11 @@
 import torch
+import torchgeometry as tgm
 from lib.models.smpl import SMPL_MODEL_DIR#, SMPL
 from smplx import SMPL # Original SMPL model
+import os
+import os.path as osp
+import joblib
+import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -20,10 +25,11 @@ if __name__ == "__main__":
     )
 
     # betas = torch.tensor(data["betas"], dtype=torch.float32)
-    axis_angle = torch.tensor(data["pose"], dtype=torch.float32)
+    axis_angle = torch.tensor(data["joints3d"], dtype=torch.float32)
 
-    # Tramsform joints to quaternions
-    quats = transforms.axis_angle_to_quaternion(axis_angle)
-    print(quats)
-    # print(f"Saving quaternion results to \'{os.path.join(output_path, 'mpsnet_output.pkl')}\'.")
-    # joblib.dump(running_results, os.path.join(output_path, "mpsnet_output.pkl"))
+    # Transform joints to quaternions
+    print("Axis-Angle Tensor Shape:", axis_angle.shape)
+    quats = tgm.angle_axis_to_quaternion(axis_angle)
+    print("Quaternion Tensor Shape:", quats.shape)
+    print(f"Saving quaternion results to \'{os.path.join(path, 'mpsnet_quats.pkl')}\'.")
+    joblib.dump(quats, os.path.join(path, "mpsnet_output.pkl"))
